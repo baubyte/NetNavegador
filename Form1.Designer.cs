b3eeae0e158ee575a7d8d19f78a7e21fb7c15742
BAUBYTE <paredbaez.martin@gmail.com>
2020-04-04 11:19:53 -0300

    Navegador con .NET

---
+++ Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace Navegador
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bBuscar = new System.Windows.Forms.Button();
+            this.tBuscar = new System.Windows.Forms.TextBox();
+            this.textBuscar = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // bBuscar
+            // 
+            this.bBuscar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(154)))), ((int)(((byte)(176)))), ((int)(((byte)(254)))));
+            this.bBuscar.Font = new System.Drawing.Font("Lucida Calligraphy", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bBuscar.Location = new System.Drawing.Point(581, 12);
+            this.bBuscar.Name = "bBuscar";
+            this.bBuscar.Size = new System.Drawing.Size(207, 31);
+            this.bBuscar.TabIndex = 0;
+            this.bBuscar.Text = "Buscar";
+            this.bBuscar.UseVisualStyleBackColor = false;
+            this.bBuscar.Click += new System.EventHandler(this.bBuscar_Click);
+            // 
+            // tBuscar
+            // 
+            this.tBuscar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(221)))), ((int)(((byte)(221)))), ((int)(((byte)(221)))));
+            this.tBuscar.Location = new System.Drawing.Point(218, 18);
+            this.tBuscar.Name = "tBuscar";
+            this.tBuscar.Size = new System.Drawing.Size(357, 20);
+            this.tBuscar.TabIndex = 1;
+            this.tBuscar.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBuscar
+            // 
+            this.textBuscar.AutoSize = true;
+            this.textBuscar.Font = new System.Drawing.Font("Lucida Calligraphy", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBuscar.Location = new System.Drawing.Point(8, 17);
+            this.textBuscar.Name = "textBuscar";
+            this.textBuscar.Size = new System.Drawing.Size(204, 21);
+            this.textBuscar.TabIndex = 2;
+            this.textBuscar.Text = "Escriba su Busqueda";
+            this.textBuscar.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(92)))), ((int)(((byte)(255)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBuscar);
+            this.Controls.Add(this.tBuscar);
+            this.Controls.Add(this.bBuscar);
+            this.Name = "Form1";
+            this.Text = "Buscador Google";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bBuscar;
+        private System.Windows.Forms.TextBox tBuscar;
+        private System.Windows.Forms.Label textBuscar;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Navegador
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bBuscar_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("www.google.com.ar");  
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
