2ab22b6e1a7512575cf6710418fe453e7a60b372
BAUBYTE <paredbaez.martin@gmail.com>
2020-04-04 16:32:06 -0300

    Buscador C# y VS

---
+++ NavegadorVS/Form1.Designer.vb
@@ -0,0 +1,78 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.bBuscar = New System.Windows.Forms.Button()
+        Me.tBuscar = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'bBuscar
+        '
+        Me.bBuscar.BackColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.bBuscar.Font = New System.Drawing.Font("Comic Sans MS", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.bBuscar.Location = New System.Drawing.Point(566, 86)
+        Me.bBuscar.Name = "bBuscar"
+        Me.bBuscar.Size = New System.Drawing.Size(82, 31)
+        Me.bBuscar.TabIndex = 0
+        Me.bBuscar.Text = "Buscar"
+        Me.bBuscar.UseVisualStyleBackColor = False
+        '
+        'tBuscar
+        '
+        Me.tBuscar.BackColor = System.Drawing.Color.Silver
+        Me.tBuscar.Location = New System.Drawing.Point(192, 92)
+        Me.tBuscar.Name = "tBuscar"
+        Me.tBuscar.Size = New System.Drawing.Size(358, 20)
+        Me.tBuscar.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Comic Sans MS", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(12, 85)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(174, 27)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Ingrese Busqueda"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(54, Byte), Integer), CType(CType(252, Byte), Integer), CType(CType(69, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(719, 242)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.tBuscar)
+        Me.Controls.Add(Me.bBuscar)
+        Me.Name = "Form1"
+        Me.Text = "Buscador"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents bBuscar As Button
+    Friend WithEvents tBuscar As TextBox
+    Friend WithEvents Label1 As Label
+End Class
---
+++ NavegadorVS/Form1.vb
@@ -0,0 +1,9 @@
+﻿Public Class Form1
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub bBuscar_Click(sender As Object, e As EventArgs) Handles bBuscar.Click
+        System.Diagnostics.Process.Start("https://www.google.com.ar/search?sxsrf=ALeKk008WtCMJQ30Fh9B3k-XMtBeGDQLvA%3A1586010186706&source=hp&ei=SpiIXvyaKbnZ5OUPqImMoA8&q=" & tBuscar.Text & "&oq=" & tBuscar.Text & "&gs_lcp=CgZwc3ktYWIQAzIHCAAQgwEQQzICCAAyAggAMgIIADIFCAAQgwEyAggAMgIIADICCAAyAggAMgIIADoECCMQJzoGCCMQJxATOgQIABBDShMIFxIPMGcxNTVnMTQ0ZzUtMjc3Sg0IGBIJMGcxZzFnNS0xUKoLWIYRYOEUaABwAHgCgAHyBIgBmQySAQswLjIuMS4wLjEuMZgBAKABAaoBB2d3cy13aXo&sclient=psy-ab&ved=0ahUKEwj856Wu_M7oAhW5LLkGHagEA_QQ4dUDCAY&uact=5")
+    End Sub
+End Class
