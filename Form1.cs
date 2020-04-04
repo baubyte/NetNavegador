34ea1a4c76e6f5dc054160dde31304c843cc0949
BAUBYTE <paredbaez.martin@gmail.com>
2020-04-04 11:34:35 -0300

    Navegador con .NET

--- Form1.cs
+++ Form1.cs
@@ -29,7 +29,7 @@ namespace Navegador
 
         private void bBuscar_Click(object sender, EventArgs e)
         {
-            System.Diagnostics.Process.Start("www.google.com.ar");  
+            System.Diagnostics.Process.Start("https://www.google.com.ar/search?sxsrf=ALeKk008WtCMJQ30Fh9B3k-XMtBeGDQLvA%3A1586010186706&source=hp&ei=SpiIXvyaKbnZ5OUPqImMoA8&q=" + tBuscar.Text + "&oq=" + tBuscar.Text + "&gs_lcp=CgZwc3ktYWIQAzIHCAAQgwEQQzICCAAyAggAMgIIADIFCAAQgwEyAggAMgIIADICCAAyAggAMgIIADoECCMQJzoGCCMQJxATOgQIABBDShMIFxIPMGcxNTVnMTQ0ZzUtMjc3Sg0IGBIJMGcxZzFnNS0xUKoLWIYRYOEUaABwAHgCgAHyBIgBmQySAQswLjIuMS4wLjEuMZgBAKABAaoBB2d3cy13aXo&sclient=psy-ab&ved=0ahUKEwj856Wu_M7oAhW5LLkGHagEA_QQ4dUDCAY&uact=5");
         }
 
         private void Form1_Load(object sender, EventArgs e)
