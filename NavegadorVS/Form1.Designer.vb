ca48763a52a040bbdc1278c223f8e6b477eed9fd
BAUBYTE <paredbaez.martin@gmail.com>
2020-04-11 14:53:52 -0300

    Add Function WebBrowser VB

--- NavegadorVS/Form1.Designer.vb
+++ NavegadorVS/Form1.Designer.vb
@@ -25,28 +25,32 @@ Partial Class Form1
         Me.bBuscar = New System.Windows.Forms.Button()
         Me.tBuscar = New System.Windows.Forms.TextBox()
         Me.Label1 = New System.Windows.Forms.Label()
-        Me.Label2 = New System.Windows.Forms.Label()
+        Me.lBuscar = New System.Windows.Forms.Label()
         Me.bRojo = New System.Windows.Forms.Button()
         Me.bAzul = New System.Windows.Forms.Button()
         Me.bVerde = New System.Windows.Forms.Button()
         Me.bVer = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.bIr = New System.Windows.Forms.Button()
+        Me.lIr = New System.Windows.Forms.Label()
+        Me.WebBrowser1 = New System.Windows.Forms.WebBrowser()
         Me.SuspendLayout()
         '
         'bBuscar
         '
         Me.bBuscar.BackColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer))
         Me.bBuscar.Font = New System.Drawing.Font("Comic Sans MS", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.bBuscar.Location = New System.Drawing.Point(566, 86)
+        Me.bBuscar.Location = New System.Drawing.Point(205, 72)
         Me.bBuscar.Name = "bBuscar"
-        Me.bBuscar.Size = New System.Drawing.Size(82, 31)
+        Me.bBuscar.Size = New System.Drawing.Size(152, 31)
         Me.bBuscar.TabIndex = 0
-        Me.bBuscar.Text = "Buscar"
+        Me.bBuscar.Text = "Buscar en Google"
         Me.bBuscar.UseVisualStyleBackColor = False
         '
         'tBuscar
         '
         Me.tBuscar.BackColor = System.Drawing.Color.Silver
-        Me.tBuscar.Location = New System.Drawing.Point(192, 92)
+        Me.tBuscar.Location = New System.Drawing.Point(188, 44)
         Me.tBuscar.Name = "tBuscar"
         Me.tBuscar.Size = New System.Drawing.Size(358, 20)
         Me.tBuscar.TabIndex = 1
@@ -55,31 +59,31 @@ Partial Class Form1
         '
         Me.Label1.AutoSize = True
         Me.Label1.Font = New System.Drawing.Font("Comic Sans MS", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.Location = New System.Drawing.Point(12, 85)
+        Me.Label1.Location = New System.Drawing.Point(8, 37)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(174, 27)
         Me.Label1.TabIndex = 2
         Me.Label1.Text = "Ingrese Busqueda"
         '
-        'Label2
+        'lBuscar
         '
-        Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(563, 120)
-        Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(90, 13)
-        Me.Label2.TabIndex = 3
-        Me.Label2.Text = "Click para Buscar"
+        Me.lBuscar.AutoSize = True
+        Me.lBuscar.Location = New System.Drawing.Point(228, 106)
+        Me.lBuscar.Name = "lBuscar"
+        Me.lBuscar.Size = New System.Drawing.Size(90, 13)
+        Me.lBuscar.TabIndex = 3
+        Me.lBuscar.Text = "Click para Buscar"
         '
         'bRojo
         '
         Me.bRojo.BackColor = System.Drawing.Color.Red
         Me.bRojo.Font = New System.Drawing.Font("Comic Sans MS", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.bRojo.ForeColor = System.Drawing.Color.White
-        Me.bRojo.Location = New System.Drawing.Point(192, 134)
+        Me.bRojo.Location = New System.Drawing.Point(576, 44)
         Me.bRojo.Name = "bRojo"
         Me.bRojo.Size = New System.Drawing.Size(94, 29)
         Me.bRojo.TabIndex = 4
-        Me.bRojo.Text = "Rojo"
+        Me.bRojo.Text = "Fondo Rojo"
         Me.bRojo.UseVisualStyleBackColor = False
         '
         'bAzul
@@ -87,11 +91,11 @@ Partial Class Form1
         Me.bAzul.BackColor = System.Drawing.Color.Navy
         Me.bAzul.Font = New System.Drawing.Font("Comic Sans MS", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.bAzul.ForeColor = System.Drawing.Color.White
-        Me.bAzul.Location = New System.Drawing.Point(456, 134)
+        Me.bAzul.Location = New System.Drawing.Point(576, 79)
         Me.bAzul.Name = "bAzul"
         Me.bAzul.Size = New System.Drawing.Size(94, 29)
         Me.bAzul.TabIndex = 5
-        Me.bAzul.Text = "Azul"
+        Me.bAzul.Text = "Fondo Azul"
         Me.bAzul.UseVisualStyleBackColor = False
         '
         'bVerde
@@ -99,34 +103,76 @@ Partial Class Form1
         Me.bVerde.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(0, Byte), Integer))
         Me.bVerde.Font = New System.Drawing.Font("Comic Sans MS", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.bVerde.ForeColor = System.Drawing.Color.White
-        Me.bVerde.Location = New System.Drawing.Point(327, 134)
+        Me.bVerde.Location = New System.Drawing.Point(677, 44)
         Me.bVerde.Name = "bVerde"
         Me.bVerde.Size = New System.Drawing.Size(94, 29)
         Me.bVerde.TabIndex = 6
-        Me.bVerde.Text = "Verde"
+        Me.bVerde.Text = "Fondo Verde"
         Me.bVerde.UseVisualStyleBackColor = False
         '
         'bVer
         '
-        Me.bVer.Font = New System.Drawing.Font("Comic Sans MS", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.bVer.Location = New System.Drawing.Point(306, 180)
+        Me.bVer.Font = New System.Drawing.Font("Comic Sans MS", 6.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.bVer.Location = New System.Drawing.Point(676, 83)
         Me.bVer.Name = "bVer"
-        Me.bVer.Size = New System.Drawing.Size(143, 32)
+        Me.bVer.Size = New System.Drawing.Size(94, 25)
         Me.bVer.TabIndex = 7
-        Me.bVer.Text = "Ver"
+        Me.bVer.Text = "Ocultar Botones"
         Me.bVer.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Comic Sans MS", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(629, 18)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(73, 21)
+        Me.Label3.TabIndex = 8
+        Me.Label3.Text = "Opciones"
+        '
+        'bIr
+        '
+        Me.bIr.BackColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.bIr.Font = New System.Drawing.Font("Comic Sans MS", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.bIr.Location = New System.Drawing.Point(381, 72)
+        Me.bIr.Name = "bIr"
+        Me.bIr.Size = New System.Drawing.Size(152, 31)
+        Me.bIr.TabIndex = 9
+        Me.bIr.Text = "Ir a la URL"
+        Me.bIr.UseVisualStyleBackColor = False
+        '
+        'lIr
+        '
+        Me.lIr.AutoSize = True
+        Me.lIr.Location = New System.Drawing.Point(424, 105)
+        Me.lIr.Name = "lIr"
+        Me.lIr.Size = New System.Drawing.Size(63, 13)
+        Me.lIr.TabIndex = 10
+        Me.lIr.Text = "Click para Ir"
+        '
+        'WebBrowser1
+        '
+        Me.WebBrowser1.Location = New System.Drawing.Point(12, 127)
+        Me.WebBrowser1.MinimumSize = New System.Drawing.Size(20, 20)
+        Me.WebBrowser1.Name = "WebBrowser1"
+        Me.WebBrowser1.Size = New System.Drawing.Size(771, 256)
+        Me.WebBrowser1.TabIndex = 11
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(54, Byte), Integer), CType(CType(252, Byte), Integer), CType(CType(69, Byte), Integer))
-        Me.ClientSize = New System.Drawing.Size(795, 242)
+        Me.ClientSize = New System.Drawing.Size(796, 390)
+        Me.Controls.Add(Me.WebBrowser1)
+        Me.Controls.Add(Me.lIr)
+        Me.Controls.Add(Me.bIr)
+        Me.Controls.Add(Me.Label3)
         Me.Controls.Add(Me.bVer)
         Me.Controls.Add(Me.bVerde)
         Me.Controls.Add(Me.bAzul)
         Me.Controls.Add(Me.bRojo)
-        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.lBuscar)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.tBuscar)
         Me.Controls.Add(Me.bBuscar)
@@ -140,9 +186,13 @@ Partial Class Form1
     Friend WithEvents bBuscar As Button
     Friend WithEvents tBuscar As TextBox
     Friend WithEvents Label1 As Label
-    Friend WithEvents Label2 As Label
+    Friend WithEvents lBuscar As Label
     Friend WithEvents bRojo As Button
     Friend WithEvents bAzul As Button
     Friend WithEvents bVerde As Button
     Friend WithEvents bVer As Button
+    Friend WithEvents Label3 As Label
+    Friend WithEvents bIr As Button
+    Friend WithEvents lIr As Label
+    Friend WithEvents WebBrowser1 As WebBrowser
 End Class
--- NavegadorVS/Form1.vb
+++ NavegadorVS/Form1.vb
@@ -6,8 +6,50 @@
     Private Sub bBuscar_Click(sender As Object, e As EventArgs) Handles bBuscar.Click
         Dim condicion As Boolean = True
         If condicion Then
-            Label2.Text = "¡Bien Hecho!"
+            lBuscar.Text = "¡Bien Hecho!"
         End If
         System.Diagnostics.Process.Start("https://www.google.com.ar/search?sxsrf=ALeKk008WtCMJQ30Fh9B3k-XMtBeGDQLvA%3A1586010186706&source=hp&ei=SpiIXvyaKbnZ5OUPqImMoA8&q=" & tBuscar.Text & "&oq=" & tBuscar.Text & "&gs_lcp=CgZwc3ktYWIQAzIHCAAQgwEQQzICCAAyAggAMgIIADIFCAAQgwEyAggAMgIIADICCAAyAggAMgIIADoECCMQJzoGCCMQJxATOgQIABBDShMIFxIPMGcxNTVnMTQ0ZzUtMjc3Sg0IGBIJMGcxZzFnNS0xUKoLWIYRYOEUaABwAHgCgAHyBIgBmQySAQswLjIuMS4wLjEuMZgBAKABAaoBB2d3cy13aXo&sclient=psy-ab&ved=0ahUKEwj856Wu_M7oAhW5LLkGHagEA_QQ4dUDCAY&uact=5")
+    End Sub
+
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
+
+    End Sub
+
+    Private Sub bIr_Click(sender As Object, e As EventArgs) Handles bIr.Click
+        Dim condicion As Boolean = True
+        If condicion Then
+            lIr.Text = "¡Bien Hecho!"
+        End If
+        If (String.IsNullOrEmpty(tBuscar.Text)) Then
+            Return
+        End If
+        If (tBuscar.Equals("about:blank")) Then
+            Return
+        End If
+        If (Not tBuscar.Text.StartsWith("http://") And Not tBuscar.Text.StartsWith("https://")) Then
+            tBuscar.Text = "http://" + tBuscar.Text
+        End If
+        Try
+            WebBrowser1.Navigate(New Uri(tBuscar.Text))
+        Catch
+            MessageBox.Show("Se produjo un error.")
+        End Try
+    End Sub
+
+    Private Sub bRojo_Click(sender As Object, e As EventArgs) Handles bRojo.Click
+        Me.BackColor = Color.Red
+    End Sub
+
+    Private Sub bVerde_Click(sender As Object, e As EventArgs) Handles bVerde.Click
+        Me.BackColor = Color.Green
+    End Sub
+
+    Private Sub bAzul_Click(sender As Object, e As EventArgs) Handles bAzul.Click
+        Me.BackColor = Color.Blue
+    End Sub
+
+    Private Sub bVer_Click(sender As Object, e As EventArgs) Handles bVer.Click
+        bBuscar.Visible = Not bBuscar.Visible
+        bIr.Visible = Not bIr.Visible
     End Sub
 End Class
