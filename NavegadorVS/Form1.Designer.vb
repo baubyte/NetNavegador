24b7cec2582eec5cf80a181b120689c8481cabed
BAUBYTE <paredbaez.martin@gmail.com>
2020-04-11 11:57:59 -0300

    Condicion If VS

--- NavegadorVS/Form1.Designer.vb
+++ NavegadorVS/Form1.Designer.vb
@@ -25,6 +25,7 @@ Partial Class Form1
         Me.bBuscar = New System.Windows.Forms.Button()
         Me.tBuscar = New System.Windows.Forms.TextBox()
         Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'bBuscar
@@ -56,12 +57,22 @@ Partial Class Form1
         Me.Label1.TabIndex = 2
         Me.Label1.Text = "Ingrese Busqueda"
         '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(563, 120)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(90, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Click para Buscar"
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(54, Byte), Integer), CType(CType(252, Byte), Integer), CType(CType(69, Byte), Integer))
         Me.ClientSize = New System.Drawing.Size(719, 242)
+        Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.tBuscar)
         Me.Controls.Add(Me.bBuscar)
@@ -75,4 +86,5 @@ Partial Class Form1
     Friend WithEvents bBuscar As Button
     Friend WithEvents tBuscar As TextBox
     Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
 End Class
