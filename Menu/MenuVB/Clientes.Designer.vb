76e6c3500437352815a20e5db857952a87619131
BAUBYTE <paredbaez.martin@gmail.com>
2020-04-18 12:53:01 -0300

    Menu en VB

--- Menu/MenuVB/Clientes.Designer.vb
+++ Menu/MenuVB/Clientes.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class Form1
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Clientes
     Inherits System.Windows.Forms.Form
 
     'Form reemplaza a Dispose para limpiar la lista de componentes.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,21 +20,33 @@ Partial Class Form1
     'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
     'Se puede modificar usando el Diseñador de Windows Forms.  
     'No lo modifique con el editor de código.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Me.bVolver = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
-        'Form1
+        'bVolver
+        '
+        Me.bVolver.Font = New System.Drawing.Font("Comic Sans MS", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.bVolver.Location = New System.Drawing.Point(689, 402)
+        Me.bVolver.Name = "bVolver"
+        Me.bVolver.Size = New System.Drawing.Size(99, 36)
+        Me.bVolver.TabIndex = 0
+        Me.bVolver.Text = "Volver"
+        Me.bVolver.UseVisualStyleBackColor = True
+        '
+        'Clientes
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
         Me.ControlBox = False
-        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
-        Me.Name = "Form1"
-        Me.Text = "Form1"
+        Me.Controls.Add(Me.bVolver)
+        Me.Name = "Clientes"
+        Me.Text = "Clientes"
         Me.ResumeLayout(False)
 
     End Sub
 
+    Friend WithEvents bVolver As Button
 End Class
---
+++ Menu/MenuVB/Form1.vb
@@ -0,0 +1,25 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles bSalir.Click
+        If MessageBox.Show("¿Seguro que Quieres Salir?", "Salir del Sistema", MessageBoxButtons.YesNo, MessageBoxIcon.Error, MessageBoxDefaultButton.Button2) = DialogResult.Yes Then End
+    End Sub
+
+    Private Sub bClientes_Click(sender As Object, e As EventArgs) Handles bClientes.Click
+        Dim cliente As New Clientes
+        cliente.BackColor = Color.Red
+        cliente.ShowDialog()
+    End Sub
+
+    Private Sub SistemaToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SistemaToolStripMenuItem.Click
+
+    End Sub
+
+    Private Sub SalirToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SalirToolStripMenuItem.Click
+        If MessageBox.Show("¿Seguro que Quieres Salir?", "Salir del Sistema", MessageBoxButtons.YesNo, MessageBoxIcon.Error, MessageBoxDefaultButton.Button2) = DialogResult.Yes Then End
+    End Sub
+
+    Private Sub ClientesToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ClientesToolStripMenuItem.Click
+        Dim cliente As New Clientes
+        cliente.BackColor = Color.Red
+        cliente.ShowDialog()
+    End Sub
+End Class
